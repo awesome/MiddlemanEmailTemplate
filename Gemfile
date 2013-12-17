0213b00c4959e648762071a3c7e84da4ad92b0c3
Brian Graves <git@brianleegraves.com>
2013-12-16 22:01:32 -0600

    premailer

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@
 source 'http://rubygems.org'
 
 gem 'middleman', '~>3.2.0'
-gem 'rubyzip'
 
 require 'rbconfig'
 gem 'wdm', '>= 0.1.0' if RbConfig::CONFIG['target_os'] =~ /mswin|mingw/i
--- config.rb
+++ config.rb
@@ -1,24 +1,6 @@
-require 'zip'
-
 compass_config do |config|
   config.output_style = :compressed
   config.line_comments = false
-end
-
-class ZipImages < Middleman::Extension
-  def initialize(app, options_hash={}, &block)
-    super
-    app.after_build do |builder|
-      directory = build_dir + "/images/"
-      zipfile_name = build_dir + "/images.zip"
-      
-      Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-          Dir[File.join(directory, '**', '**')].each do |file|
-            zipfile.add(file.sub(directory, ''), file)
-          end
-      end
-    end
-  end
 end
 
 set :css_dir, 'stylesheets'
@@ -26,44 +8,46 @@ set :images_dir, 'images'
 #set :build_dir, 'B:/SetDirectory'
 set :is_building, false
 set :et_content_dir, "Content\\"
-set :add_regions, false
 
 configure :build do
   activate :minify_css
   activate :relative_assets
-  # activate :zip_images
   set :is_building, true
 end
+
+module PreMailer
+  class << self
+    def registered(app)
+      require "premailer"
+      app.after_build do |builder|
+        prefix = build_dir + File::SEPARATOR
+        Dir.chdir(build_dir) do
+          Dir.glob('**/*.html') do |file|
+            premailer = Premailer.new(file, :adapter => :nokogiri, :preserve_styles => false, :remove_comments => true, :remove_ids => true)
+            fileout = File.open(file, "w")
+            fileout.puts premailer.to_inline_css
+            fileout.close
+            builder.say_status :premailer, prefix+file
+          end
+        end
+      end
+    end
+    alias :included :registered
+  end
+end
+::Middleman::Extensions.register(:inline_premailer, PreMailer)
+activate :inline_premailer
 
 helpers do
   set :contentAreaCounter, 0
   def content_area(area)
     set :contentAreaCounter, contentAreaCounter + 1
-    if ( is_building == true )
-      concat '<custom type="content" name="contentarea' + contentAreaCounter.to_s + '">'
-    else
-      if ( add_regions && add_regions == true )
-        region(area) do
-          concat partial("content/" + area)
-        end
-      else
-        concat partial("content/" + area)
-      end
-    end
+    concat is_building == true ? '<custom type="content" name="contentarea' + contentAreaCounter.to_s + '">' : partial("content/" + area)
   end
   def page_data(ns, var)
     concat is_building == true ? "%%=v(@" + ns + "_" + var + ")=%%" : data.template[ns][var]
   end
   def page_link(ns, var)
     concat is_building == true ? "%%=redirectto(@" + ns + "_" + var + ")=%%" : data.template[ns][var]
-  end
-  def region regionName, force=false
-    if ( is_building == true || force == true )
-      concat "%%=BeginImpressionRegion(\"" + regionName + "\")=%%"
-      yield
-      concat "%%=EndImpressionRegion()=%%"
-    else
-      yield 
-    end
   end
 end
